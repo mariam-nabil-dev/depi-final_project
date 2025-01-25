e8a3c1674f44f4f686a5ad19e59f0c7c7dfb3668
alaaabubakr <alaaabubakr22@yahoo.com>
2025-01-25 10:17:07 +0200

    alaa

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,5 +1,14 @@
-const About = () => {
-  return <div>About</div>;
-};
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
 
-export default About;
+import Aboutsections from './../../components/Aboutsections/Aboutsections';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <StrictMode>
+    <Aboutsections />
+  </StrictMode>,
+)
