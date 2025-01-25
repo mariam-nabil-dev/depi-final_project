118e070380f55101ac8b038113680917c7f23fe5
Andrew Nashaat <166779741+Zaroscript@users.noreply.github.com>
2025-01-25 13:24:13 +0200

    Update About.jsx

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { StrictMode } from 'react';
-
 import Aboutfirst from '../../components/Aboutsections/Aboutfirst.jsx';
 import Aboutsections from '../../components/Aboutsections/Aboutsections.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <StrictMode>
-    <Aboutfirst/>
-    <Aboutsections />
-  </StrictMode>,
-)
+export default function About(){
+ return (
+    <>
+      <Aboutfirst/>
+      <Aboutsections />
+    </>
+   )
+}
