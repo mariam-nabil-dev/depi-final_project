60414976ee7fabd934b7ab48bae683b7e57c3c1b
alaaabubakr <alaaabubakr22@yahoo.com>
2025-01-25 11:36:48 +0200

    About first

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
 
-import Aboutsections from './../../components/Aboutsections/Aboutsections';
-
-
+import Aboutfirst from '../../components/Aboutsections/Aboutfirst.jsx';
+import Aboutsections from '../../components/Aboutsections/Aboutsections.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
+    <Aboutfirst/>
     <Aboutsections />
   </StrictMode>,
 )
