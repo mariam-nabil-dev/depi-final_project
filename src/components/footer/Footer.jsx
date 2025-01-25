fb9b49c9f9fecc1a1dbe8293f030a2b5579030cc
Andrew Nashaat <166779741+Zaroscript@users.noreply.github.com>
2025-01-25 17:16:26 +0200

    Upload the navbar and footer first version

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react'
 
 const Footer = () => {
@@ -7,3 +8,67 @@ const Footer = () => {
 }
 
 export default Footer
+=======
+import { Container, Nav } from "react-bootstrap";
+import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <Container>
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        {/* Logo or Brand Link */}
+        <a
+          href="/"
+          className="col-md-2 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+        >
+          <img src="/logo.svg" width={50} height={50} alt="Polaris Logo" />
+        </a>
+
+        {/* Navigation Links */}
+        <Nav className="col-md-8 justify-content-center">
+          {[
+            { name: "Home", path: "/" },
+            { name: "About", path: "/about" },
+            { name: "Categories", path: "/categories" },
+            { name: "Blogs", path: "/blogs" },
+            { name: "News", path: "/news" },
+            { name: "Contact", path: "/contact" },
+          ].map((link, index) => (
+            <Nav.Item key={index}>
+              <Nav.Link as={NavLink} to={link.path} className="px-2 text-body-secondary">
+                {link.name}
+              </Nav.Link>
+            </Nav.Item>
+          ))}
+        </Nav>
+
+        {/* Social Media Icons */}
+        <div className="col-md-2 d-flex justify-content-end">
+          {[
+            { icon: <FaFacebook />, link: "#" },
+            { icon: <FaTwitter />, link: "#" },
+            { icon: <FaInstagram />, link: "#" },
+            { icon: <FaGithub />, link: "#" },
+          ].map((social, index) => (
+            <a
+              key={index}
+              href={social.link}
+              className="px-2 text-body-secondary"
+            >
+              {social.icon}
+            </a>
+          ))}
+        </div>
+
+        {/* Copyright Text */}
+        <p className="col-md-12 text-center mt-2 mb-0 text-body-secondary">
+          &copy; 2025 Polaris, All rights reserved
+        </p>
+      </footer>
+    </Container>
+  );
+};
+
+export default Footer;
+>>>>>>> Stashed changes
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { IoHomeOutline } from "react-icons/io5";
 import { BsInfoSquare } from "react-icons/bs";
 import { IoNewspaperOutline } from "react-icons/io5";
@@ -56,6 +57,51 @@ const NavBar = () => {
             className="d-inline-block align-center"
           />{" "}
           InnoCrew
+=======
+import { useState } from "react";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Button,
+  Container,
+  Offcanvas,
+} from "react-bootstrap";
+import { FaGlobe, FaMoon, FaSun, FaUser } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import "./navbar.css"
+
+const navLinks = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Categories", path: "/categories" },
+  { name: "Blogs", path: "/blogs" },
+  { name: "News", path: "/news" },
+  { name: "Contact", path: "/contact" },
+];
+
+const NavBar = () => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    document.body.setAttribute("data-theme", newTheme);
+  };
+
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary mb-3" sticky="top">
+      <Container fluid className="px-4">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/logo.svg"
+            width="40"
+            height="40"
+            className="d-inline-block align-center"
+          />{" "}
+          <span className="font-paragraph fw-medium fs-4">Polaris</span>
+>>>>>>> Stashed changes
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" />
         <Navbar.Offcanvas
@@ -65,11 +111,16 @@ const NavBar = () => {
         >
           <Offcanvas.Header closeButton>
             <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">
+<<<<<<< Updated upstream
               InnoCrew
+=======
+              Polaris
+>>>>>>> Stashed changes
             </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
             <Nav
+<<<<<<< Updated upstream
               className="justify-content-center flex-grow-1 pe-3 gap-2 fs-6"
               defaultActiveKey="/"
             >
@@ -80,10 +131,18 @@ const NavBar = () => {
                   to={link.path}
                   className="rounded-2 p-2 text-center"
                 >
+=======
+              className="justify-content-center flex-grow-1 pe-3 gap-2"
+              defaultActiveKey="/"
+            >
+              {navLinks.map((link, index) => (
+                <Nav.Link key={index} as={NavLink} to={link.path} className="navbar-link">
+>>>>>>> Stashed changes
                   {link.name}
                 </Nav.Link>
               ))}
             </Nav>
+<<<<<<< Updated upstream
             <Form className="d-flex search font-primary mt-4 mt-md-0">
               <Form.Control
                 type="search"
@@ -95,6 +154,44 @@ const NavBar = () => {
                 <IoSearch />
               </Button>
             </Form>
+=======
+
+            <div className="d-flex align-items-center gap-4">
+              {/* Profile Dropdown */}
+              <NavDropdown title={<FaUser />} id="dropdown-profile" align="end">
+                <NavDropdown.Item as={NavLink} to="/profile">
+                  My Profile
+                </NavDropdown.Item>
+                <NavDropdown.Item as={NavLink} to="/settings">
+                  Settings
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#logout">Logout</NavDropdown.Item>
+              </NavDropdown>
+
+              {/* Language Dropdown */}
+              <NavDropdown
+                title={<FaGlobe />}
+                id="dropdown-language"
+                className=""
+                align="end"
+              >
+                <NavDropdown.Item>Arabic (AR)</NavDropdown.Item>
+                <NavDropdown.Item>English (EN)</NavDropdown.Item>
+                <NavDropdown.Item>Español (ES)</NavDropdown.Item>
+                <NavDropdown.Item>Français (FR)</NavDropdown.Item>
+              </NavDropdown>
+
+              {/* Theme Toggle Button */}
+              <Button
+                variant="link"
+                onClick={toggleTheme}
+                className="theme-toggle"
+              >
+                {theme === "light" ? <FaMoon /> : <FaSun />}
+              </Button>
+            </div>
+>>>>>>> Stashed changes
           </Offcanvas.Body>
         </Navbar.Offcanvas>
       </Container>
