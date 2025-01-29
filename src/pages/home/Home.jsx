e183ffc6ecbd6541f4a945eab34bc946f2c4e658
mariam-nabil-dev <167152750+mariam-nabil-dev@users.noreply.github.com>
2025-01-29 02:36:31 +0200

    create footer content & travel tips components (#7)

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,10 @@
+import TravelTipsForm from "../../components/TravelTipsForm/TravelTipsForm";
 
 const Home = () => {
   return (
     <>
       <h1>Home</h1>
+      <TravelTipsForm />
     </>
   );
 };
